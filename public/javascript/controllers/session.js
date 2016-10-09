80bfe644ec9bd0e308358e973166904c9fd75cf7
Dax <limeside@gmail.com>
2016-10-09 11:43:33 -0500

    Edit show controller, checks for host. :rabbit:

--- public/javascript/controllers/session.js
+++ public/javascript/controllers/session.js
@@ -25,19 +25,22 @@
     var vm = this;
     vm.getUser = Users.getUser;
     vm.sessionSkill = sessionSkill;
+    vm.isHost = false;
     Sessions
       .getSession($routeParams.session_id)
       .then( session => {
         vm.info = session.data;
-      })
-
-    Users
-      .getCurrentUser()
-      .then( results => {
-        let user = results ? results.data : null;
-
-        user ? vm.currentUser = user : vm.currentUser = null
-        console.log(vm.currentUser);
+      }).then( () => {
+        Users
+          .getCurrentUser()
+          .then( results => {
+            console.log(results.data.id);
+            vm.currentUser = results.data.id ? results.data : {id: 0};
+            if(vm.currentUser.id === vm.info.host.id) {
+              console.log('isHost');
+              vm.isHost = true;
+            }
+          })
       })
 
 
