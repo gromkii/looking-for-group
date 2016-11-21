ea7612031c7e291fcaf4aaad784fd3a7d2c29810
Dax <limeside@gmail.com>
2016-11-20 21:41:48 -0600

    Edit ui-state, keeps fucking up, great. :rabbit:

--- public/javascript/controllers/dashboard.js
+++ public/javascript/controllers/dashboard.js
@@ -1,7 +1,24 @@
 (function(){
   angular
     .module('dashboard', [])
+    .config(routeConfig)
     .controller('DashboardController', DashboardController);
+
+    routeConfig.$inject = ['$routeProvider', '$locationProvider'];
+
+    function routeConfig($routeProvider, $locationProvider) {
+      $routeProvider
+        .when('/dashboard', {
+          controller:'DashboardController',
+          controllerAs:'dashboard',
+          templateUrl:'views/dashboard/index.html'
+        })
+
+      $locationProvider.html5Mode({
+        enabled:true,
+        requireBase:false
+      })
+    }
 
     function DashboardController(){
       let vm = this;
--- public/javascript/controllers/index.js
+++ public/javascript/controllers/index.js
@@ -1,9 +1,25 @@
 (function(){
   angular
     .module('index', ['Users'])
+    .config(routeConfig)
     .controller('IndexController', IndexController);
 
+  routeConfig.$inject  = ['$routeProvider', '$locationProvider']
   IndexController.$inject = ['Users']
+
+  function routeConfig($routeProvider, $locationProvider) {
+    $routeProvider
+      .when('/', {
+        controller:'IndexController',
+        controllerAs:'index',
+        templateUrl:'views/index/index.html'
+      })
+
+    $locationProvider.html5Mode({
+      enabled:true,
+      requireBase:false
+    })
+  }
 
   function IndexController(Users){
     var vm = this;
