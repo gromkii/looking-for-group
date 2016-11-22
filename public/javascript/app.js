516c4ca0f1269d290408a4b92c6943120698a825
Dax <limeside@gmail.com>
2016-11-22 17:59:09 -0600

    Refactor search for session view. :rabbit:

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -27,32 +27,4 @@
       'Chats'
      ]
    )
-  // .config(stateConfig)
-  //
-  // stateConfig.$inject = ["$stateProvider", "$urlRouterProvider"];
-  //
-  // function stateConfig($stateProvider, $urlRouterProvider) {
-  //   $stateProvider
-  //     .state('index', {
-  //       url:'/',
-  //       controller:'IndexController',
-  //       controllerAs:'index',
-  //       templateUrl:'views/index/index.html'
-  //     })
-  //
-  //     .state('dashboard', {
-  //       url:'/dashboard',
-  //       controller:'DashboardController',
-  //       controllerAs:'dashboard',
-  //       templateUrl:'views/dashboard/index.html'
-  //     })
-  //
-  //     .state('sessions.show', {
-  //       url:'/sessions/:session_id',
-  //       templateUrl:'views/dashboard/sessions/show.html',
-  //       controller:'SessionController',
-  //       controllerAs:'session'
-  //     })
-  // }
-
 })();
